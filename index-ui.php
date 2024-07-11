9a9d398a2a56b778e6bbc2bce6a68af293440c6f
frootyjas <jasminedenriquez@gmail.com>
2024-07-11 12:18:41 +0800

    Update index-ui.php

--- index-ui.php
+++ index-ui.php
@@ -10,7 +10,7 @@
         <div class="upper-content-container">
             <div class="logo-container">
                 <a href="index.php">
-                    <img src="../content-images/logo-white.png" alt="Logo">
+                    <img src="assets/content-images/logo-white.png" alt="Logo">
                 </a>
             </div>
             <div class="title-container">
@@ -20,7 +20,7 @@
     </div>
 
     <div class="img-container">
-        <img src="../content-images/indexImg.png" alt="Image">
+        <img src="assets/content-images/indexImg.png" alt="Image">
     </div>
 </body>
 </html>
