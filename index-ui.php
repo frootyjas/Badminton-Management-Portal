7478d63fd5d82abfcedbf3d21c24a7ade5c40a2b
frootyjas <jasminedenriquez@gmail.com>
2024-07-11 12:24:25 +0800

    Update index-ui.php

--- index-ui.php
+++ index-ui.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/index-ui.css">
+<link rel="stylesheet" href="src/css/index-ui.css">
 </head>
 <body>
     <div class="container">
